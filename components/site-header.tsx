d97bebf4cdce2454b5c4a30db59e23944379ebed
Sid Shelke <ausv2027@gmail.com>
2023-12-28 00:02:51 +0530

    deployment

---
+++ components/site-header.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { Edit, ShoppingBag } from "lucide-react"
+import { useShoppingCart } from "use-shopping-cart"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { MainNav } from "@/components/main-nav"
+import { ThemeToggle } from "@/components/theme-toggle"
+
+export function SiteHeader() {
+  const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const { cartCount } = useShoppingCart()
+  const defaultSearch = searchParams.get("search") ?? ""
+  if (pathname.startsWith("/studio")) return null
+
+  function onSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const searchQuery = formData.get("search")
+    router.replace(`/?search=${searchQuery}`)
+  }
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
+        <MainNav />
+        <form
+          onSubmit={onSubmit}
+          className="hidden items-center lg:inline-flex"
+        >
+          <Input
+            id="search"
+            name="search"
+            type="search"
+            autoComplete="off"
+            placeholder="Search products..."
+            className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearch}
+          />
+        </form>
+        <div className="flex items-center space-x-1">
+          <Link href="/cart">
+            <Button size="sm" variant="ghost">
+              <ShoppingBag className="h-5 w-5" />
+              <span className="ml-2 text-sm font-bold">{cartCount}</span>
+              <span className="sr-only">Cart</span>
+            </Button>
+          </Link>
+          <ThemeToggle />
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ sanity/lib/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@sanity/client"
+
+import { apiVersion, dataset, projectId, useCdn } from "../env"
+
+export const client = createClient({
+  apiVersion,
+  dataset,
+  projectId,
+  useCdn,
+})
